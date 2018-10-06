778a681c0c61b80e13df78efe08b0901a028b1cc
Jerry Edebua <jeredebua@gmail.com>
2018-10-06 17:45:59 +0300

    wrote tests

---
+++ tests.js
@@ -0,0 +1,16 @@
+/* 
+ * Tests
+ *  */
+
+var Component = require('./src/to-json');
+
+describe(`Component Tests
+`, function() {
+
+  var component = new Component();
+
+  it('Component should have all required properties', function(done) {
+    done();
+  });
+
+});
